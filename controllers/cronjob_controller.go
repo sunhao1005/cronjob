38fd84877423e2a6ceb97ea71647d554fb5aba17
sunhao1005 <2417291999@qq.com>
2022-03-02 17:42:27 +0800

    modify set

--- controllers/cronjob_controller.go
+++ controllers/cronjob_controller.go
@@ -18,6 +18,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -48,7 +49,7 @@ type CronJobReconciler struct {
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.0/pkg/reconcile
 func (r *CronJobReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
-
+	fmt.Printf("*********| Name %s | Namespace %s | NamespacedName %s |********", req.Name, req.Namespace, req.NamespacedName)
 	// TODO(user): your logic here
 
 	return ctrl.Result{}, nil
@@ -56,6 +57,8 @@ func (r *CronJobReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *CronJobReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	fmt.Println("********* Scheme: ", r.Scheme, " **********")
+	fmt.Println("********* Client.Status: ", r.Client.Status(), " **********")
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&demotestv1.CronJob{}).
 		Complete(r)
